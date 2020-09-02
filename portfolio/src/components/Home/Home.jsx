3a6a6cd6c175fa77c56d32032fe28cd927590215
Brage Rosberg <brage70@gmail.com>
2020-09-02 15:49:18 +0200

    Fixed the homepage to contain more updated information, aswell as removing gmail icon since there is a contact me page now running

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -12,8 +12,8 @@ class Home extends React.Component {
         <div>
           <h1 className="homepage__name">Brage Røsberg</h1>
           <h1 className="homepage__title">Full Stack JavaScript Developer</h1>
-          <p className="homepage__development">Page is currently under development, feel free to contact!.</p>
-          <a href="mailto:bragecontact@gmail.com"><img className="homepage__contact" alt="gmail" src="https://i.imgur.com/K9UHWV1.png"/></a>  
+          <p className="homepage__development">Located in Oslo, feel free to contact!</p>
+          <p className="homepage__development">(Page is currently under development)</p>
         </div>
        </section>
        <Techstack />
