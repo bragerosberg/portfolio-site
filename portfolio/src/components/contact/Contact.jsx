a6a0b6035032690b3a588353d97b469b365454b7
Brage Rosberg <brage70@gmail.com>
2020-10-23 17:24:38 +0200

    Added full name on contact page

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -13,6 +13,7 @@ const Contact = () => {
       <div className="contact__wrapper">
         <article className="contact__information">
           <img className="contact__portrait" src={pb} alt="profile"/>
+          <h2 className="contact__name">Brage Røsberg</h2>
           <h2 className="contact__subheader">Get in touch with me!</h2>
           <section className="contact__icons">
             <a href="mailto:bragecontact@gmail.com" target="_blank" rel="noopener noreferrer"><img src="https://image.flaticon.com/icons/svg/1000/1000885.svg" alt="mail"/></a>
