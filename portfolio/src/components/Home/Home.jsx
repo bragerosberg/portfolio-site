c5a093a50f69bee863b45d84284a039c3cc55fcd
Brage Rosberg <brage70@gmail.com>
2020-10-23 13:08:40 +0200

    Added FontAwesome icons for the main techstack on the ladningpage

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/Home.css';
 
+import portrait from "../../assets/portrait.png";
 import About from './About'
 import Techstack from './Techstack';
 
@@ -10,7 +11,7 @@ class Home extends React.Component {
     return (
      <main>
       <section className="homepage__wrapper">
-        <img className="portrait" src="https://i.imgur.com/uVpbV1Z.png" alt="portrait"/>
+        <img className="portrait" src={portrait}  alt="portrait"/>
         <div className="homepage__info__wrapper">
           <h1 className="homepage__name">Brage Røsberg</h1>
           <h1 className="homepage__title">Full Stack JavaScript Developer</h1>
--- portfolio/src/components/Home/Techstack.jsx
+++ portfolio/src/components/Home/Techstack.jsx
@@ -5,13 +5,11 @@ const Techstack = () => {
   return (
     <article>
       <section className="tech__logos">
-        <img src="https://i.imgur.com/AkbotMR.png" alt="js"/>
-        <img src="https://i.imgur.com/WCwxLaz.png" alt="react"/>
-        <img src="https://i.imgur.com/abOnPp4.png" alt="nodejs"/>
-        <img src="https://i.imgur.com/g6VzPNZ.png" alt="express"/>
-        <img src="https://i.imgur.com/2yH5rFb.png" alt="mongodb"/>
-        <img src="https://i.imgur.com/8KVSwvp.png" alt="html5"/>
-        <img src="https://i.imgur.com/GsdKN8c.png" alt="css3"/>
+        <i class="fab fa-js"></i>
+        <i class="fab fa-react"></i>
+        <i class="fab fa-node"></i>
+        <i class="fab fa-html5"></i>
+        <i class="fab fa-css3"></i>
       </section>
     </article>
   )
