2c9701ca21e62e048aa840b7179176e347d86e47
Brage Rosberg <brage70@gmail.com>
2020-10-23 15:46:28 +0200

    Updated all illustartion photos to be more realistic, using commercial free images from Pexels.com

--- portfolio/src/components/Project/projects.js
+++ portfolio/src/components/Project/projects.js
@@ -1,8 +1,9 @@
 const { uuid } = require('uuidv4');
+
 const myProjects = [
   {
     index: uuid(),
-    image: "https://i.imgur.com/CGE4Bbh.png",
+    image: "https://i.imgur.com/yoc4qE2.png",
     title: "Budget Manager (Android)",
     text: "The mobile version of another app I've made with the same concept. Manage a budget, add expenses and easily edit/delete. Values are always stored so you'll see the numbers exactly as you expect them at all times.",
     repolink: "https://github.com/bragerosberg/budgetManagerNative",
@@ -11,7 +12,7 @@ const myProjects = [
   },
   {
     index: uuid(),
-    image: "https://i.imgur.com/SV6i6vt.png",
+    image: "https://i.imgur.com/zwaYrQ1.jpg",
     title: "Day Organizer",
     text: "Adding two useful parts that greatly fits together, a board of tasks where you may write anything you'd like, while having a 5-day forecast at the top of the page. Making it easy to prepare based on the weather (Oslo).",
     repolink: "https://github.com/bragerosberg/DayOrganizer",
@@ -20,7 +21,7 @@ const myProjects = [
   },
   {
     index: uuid(),
-    image: "https://i.imgur.com/IvAKdPc.png",
+    image: "https://i.imgur.com/o8ogcY6.png",
     title: "Hackday Project (24 hours)",
     text: "An assignment I was given during my bootcamp, create a product of your choice, consisting of a frontend and-backend. Three different APIs were used, a MVP-project",
     repolink: "https://github.com/bragerosberg/Hackday",
@@ -29,7 +30,7 @@ const myProjects = [
   },
   {
     index: uuid(),
-    image: "https://i.imgur.com/MJvasqg.png",
+    image: "https://i.imgur.com/QxPaWdC.png",
     title: "Graduation Project (Android app) ",
     text: "My mob was assigned to create a project through 2 sprints, using SCRUM preperation. App is a voluntary platform where users and organisations can interact and add events. ",
     repolink: "https://github.com/jopemoma/Graduation-Project",
@@ -38,7 +39,7 @@ const myProjects = [
   },
   {
     index: uuid(),
-    image: "https://i.imgur.com/Xlnrij0.png",
+    image: "https://i.imgur.com/WVak6hX.png",
     title: "Budget Manager",
     text: "First release of this product (android version is the second). Developed for full responsiveness on web. Manage a budget, add expenses and easily edit/delete. Values are always stored so you'll see the numbers exactly as you expect them at all times.",
     repolink: "https://github.com/bragerosberg/BudgetManager",
@@ -47,7 +48,7 @@ const myProjects = [
   },
   {
     index: uuid(),
-    image: "https://i.imgur.com/urL3xOL.png",
+    image: "https://i.imgur.com/nC61JYM.png",
     title: "Vue Todo",
     text: "A todo application created in Vue. Easily create tasks, toggle as complete, delete a task, delete every completed or completely reset the list.",
     repolink: "https://github.com/bragerosberg/vuetodo",
@@ -56,7 +57,7 @@ const myProjects = [
   },
   {
     index: uuid(),
-    image: "https://github.com/bragerosberg/ReactTodo/raw/master/phone.png",
+    image: "https://i.imgur.com/ttj5XsP.png",
     title: "React Todo",
     text: "A todo application made with REACT, full responsive across platforms",
     repolink: "https://github.com/bragerosberg/ReactTodo",
