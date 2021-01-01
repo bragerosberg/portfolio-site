3bc67bd70bdf899246654a7434c9a775d716a4ed
Brage Rosberg <brage70@gmail.com>
2021-01-01 02:58:13 +0100

    Updated year in footer

--- portfolio/src/components/Home/Footer.jsx
+++ portfolio/src/components/Home/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <ul>
       <li><a href="https://www.github.com/bragerosberg" target="_blank" rel="noopener noreferrer">Github</a></li>
-      <li className="footer__author">Made by Brage Røsberg - 2020</li>
+      <li className="footer__author">Made by Brage Røsberg - 2021</li>
       
       <li><a href="https://www.linkedin.com/in/brage-rosberg" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
     </ul>
