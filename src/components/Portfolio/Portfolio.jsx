0331e9ff30757a90edae2185e2e3a140ab740083
Brage Rosberg <brage70@gmail.com>
2021-05-02 23:38:21 +0200

    Translation for portfolio page

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import './styles/Portfolio.css';
 import CV from './CV';
 import PortfolioCard from './PortfolioCard';
@@ -13,56 +14,59 @@ import {
   acknowledgement,
 } from './experience';
 
-const Portfolio = () => (
-  <main className="portfolio__page">
 
-    <h1 className="portfolio__experience--title">Knowledge</h1>
-    <section className="portfolio__experience">
+const Portfolio = () => {
+  const { t } = useTranslation();
 
-      <PortfolioCard portfolioList={frontEnd} name={'Frontend'} />
+  return (
+    <main className="portfolio__page">
 
-      <PortfolioCard portfolioList={backEnd} name={'Backend'} />
+      <h1 className="portfolio__experience--title">{t('portfolio:Knowledge')}</h1>
+      <section className="portfolio__experience">
 
-      <PortfolioCard portfolioList={languages} name={'Languages'} />
+        <PortfolioCard portfolioList={frontEnd} name={'Frontend'} />
 
-      <PortfolioCard portfolioList={tools} name={'Tools'} />
+        <PortfolioCard portfolioList={backEnd} name={'Backend'} />
 
-      <PortfolioCard portfolioList={principles} name={'Principles'} />
-      
-    </section>
- 
-    <h1 className="portfolio__wrapper--title">Experience</h1>
-    <section className="portfolio__wrapper">
+        <PortfolioCard portfolioList={languages} name={'Languages'} />
 
-      <article className="portfolio__work">
-        <h2 className="portfolio__experience--category">Jobs</h2>
-        {workExperience.map(e => <CV key={e.key} date={e.date} description={e.description} />)}
-      </article>
+        <PortfolioCard portfolioList={tools} name={'Tools'} />
 
-      <article className="portfolio__education">
-        <h2 className="portfolio__experience--category">Education</h2>
-        {education.map(e => <CV key={e.key} date={e.date} description={e.description} />)}
-      </article>
+        <PortfolioCard portfolioList={principles} name={'Principles'} />
+
+      </section>
+
+      <h1 className="portfolio__wrapper--title">{t('portfolio:Experience')}</h1>
+      <section className="portfolio__wrapper">
+
+        <article className="portfolio__work">
+          <h2 className="portfolio__experience--category">{t('portfolio:Jobs')}</h2>
+          {workExperience.map(e => <CV key={e.key} date={e.date} description={e.description} />)}
+        </article>
+
+        <article className="portfolio__education">
+          <h2 className="portfolio__experience--category">{t('portfolio:Education')}</h2>
+          {education.map(e => <CV key={e.key} date={e.date} description={e.description} />)}
+        </article>
 
 
-    </section>
+      </section>
 
-    <h1 className="portfolio__wrapper--title">Acknowledgements</h1>
-    <section className="portfolio__wrapper">
+      <h1 className="portfolio__wrapper--title">{t('portfolio:Acknowledgements')}</h1>
+      <section className="portfolio__wrapper">
 
-      <article className="portfolio__education">
-        {acknowledgement.map(e =>
-          <div key={e.key}>
-            <p>{e.description}</p>
-            {(e.link ? <a className="btn btn-light link__button" href={e.link} target="_blank" rel="noopener noreferrer">Read Article</a> : null)} 
+        <article className="portfolio__education">
+          {acknowledgement.map(e => <div key={e.key}>
+            <p>{t(`portfolio:${e.description}`)}</p>
+            {(e.link ? <a className="btn btn-light link__button" href={e.link} target="_blank" rel="noopener noreferrer">Read Article</a> : null)}
             <img src={e.img} alt={e.name} />
-          </div>)
-        }
-      </article>
+          </div>)}
+        </article>
 
-    </section>
+      </section>
 
-  </main>
-)
+    </main>
+  );
+}
 
 export default Portfolio;
