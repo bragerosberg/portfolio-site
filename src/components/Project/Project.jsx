19546af306a060519f6e7a02d7afbd2f41f2bd10
Brage Rosberg <brage70@gmail.com>
2021-05-13 17:20:57 +0200

    Update

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -15,9 +15,9 @@ const Project = () => {
         {myProjects.map(({ image, alt, title, text, topics, repolink, website }, i) => (
           <Card key={i} style={{ width: "18rem" }} className="project__card">
             <Card.Img variant="top" className="project__image" src={image} alt={alt}/>
-            <Card.Title className="project__title">{title}</Card.Title>
-            <Card.Text className="project__text">{text}</Card.Text>
-            <Card.Footer className="project__topic">Tools: {topics}</Card.Footer>
+            <Card.Title className="project__title">{t(`project:${title}`)}</Card.Title>
+            <Card.Text className="project__text">{t(`project:${text}`)}</Card.Text>
+            <Card.Footer className="project__topic">{t('project:Tools')}: {topics}</Card.Footer>
             <div className="button__wrapper">
               <a 
                 className="btn btn-light link__button"
