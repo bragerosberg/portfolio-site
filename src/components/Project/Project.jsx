c51edd64ca228a93323d02098ca150357861d944
Brage Røsberg <brage70@gmail.com>
2021-06-16 22:25:50 +0200

    Update projects header font

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -9,42 +9,53 @@ const Project = () => {
 
   return (
     <main>
-      <h1 className="project__title">{t('project:PersonalProjects')}</h1>
+      <h1 className="portfolio__experience--title">
+        {t('project:PersonalProjects')}
+      </h1>
 
       <section className="project__wrapper">
-        {myProjects.map(({ image, alt, title, text, topics, repolink, website }, i) => (
-          <Card key={i} style={{ width: "18rem" }} className="project__card">
-            <Card.Img variant="top" className="project__image" src={image} alt={alt}/>
-            <Card.Title className="project__title">{t(`project:${title}`)}</Card.Title>
-            <Card.Text className="project__text">{t(`project:${text}`)}</Card.Text>
-            <Card.Footer className="project__topic">{topics}</Card.Footer>
-            <div className="button__wrapper">
-              <a 
-                className="btn btn-light link__button"
-                href={repolink}
-                target="_blank"
-                rel="noopener noreferrer">
-                  Repository
-              </a>
-              {(
-                website ? 
-                <a 
+        {myProjects.map(
+          ({ image, alt, title, text, topics, repolink, website }, i) => (
+            <Card key={i} style={{ width: '18rem' }} className="project__card">
+              <Card.Img
+                variant="top"
+                className="project__image"
+                src={image}
+                alt={alt}
+              />
+              <Card.Title className="project__title">
+                {t(`project:${title}`)}
+              </Card.Title>
+              <Card.Text className="project__text">
+                {t(`project:${text}`)}
+              </Card.Text>
+              <Card.Footer className="project__topic">{topics}</Card.Footer>
+              <div className="button__wrapper">
+                <a
                   className="btn btn-light link__button"
-                  href={website}
+                  href={repolink}
                   target="_blank"
-                  rel="noopener noreferrer">
-                  Website
-                </a> 
-                  : 
-                  null
-              )} 
-            </div>
-          </Card>
-        ))}
+                  rel="noopener noreferrer"
+                >
+                  Repository
+                </a>
+                {website ? (
+                  <a
+                    className="btn btn-light link__button"
+                    href={website}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Website
+                  </a>
+                ) : null}
+              </div>
+            </Card>
+          )
+        )}
       </section>
-      
     </main>
-  )
+  );
 }
 
 export default Project;
