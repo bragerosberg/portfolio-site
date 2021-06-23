d1f4cae96bfd758007dd2f93d4396cb368ba01e3
Brage Røsberg <brage70@gmail.com>
2021-06-23 16:10:57 +0200

    Update projects and portfolio page, scrollbar color

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import Techstack from './Techstack';
 import './styles/Home.css';
 
 const Home = () => {
-  const { t } = useTranslation();
   return (
     <main>
       <section className="homepage__wrapper">
         <div className="homepage__info__wrapper">
-          <h1 className="homepage__name">Brage Rosberg</h1>
+          <h1 className="homepage__name">Brage</h1>
+          <h1 className="homepage__surname">Rosberg</h1>
+          <Techstack />
         </div>
       </section>
-      {/* <Techstack /> */}
     </main>
   );
 }
