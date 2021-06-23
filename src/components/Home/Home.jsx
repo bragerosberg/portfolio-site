2be4c34392617a4a7610884ea57d958d0d91db35
Brage Røsberg <brage70@gmail.com>
2021-06-23 14:53:01 +0200

    Remove github and linkedin hot-link buttons

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,9 +3,6 @@ import { useTranslation } from 'react-i18next';
 import Techstack from './Techstack';
 import portrait from "../../assets/portrait.png";
 import './styles/Home.css';
-import Github from '../../assets/github.png';
-import Linkedin from '../../assets/linkedin.png';
-
 
 const Home = () => {
   const { t } = useTranslation();
@@ -19,16 +16,6 @@ const Home = () => {
           <p className="homepage__location">{t('home:CurrentPosition')}</p>
         </div>
       </section>
-      <div className="homepage__hotlinks">
-      
-        <a href="https://www.github.com/bragerosberg" target="_blank" rel="noopener noreferrer">
-          <img src={Github} alt="github" />
-        </a>
-        <a href="https://www.linkedin.com/in/brage-rosberg" target="_blank" rel="noopener noreferrer">
-          <img src={Linkedin} alt="linkedin" />
-        </a>
-
-      </div>
     </main>
   );
 }
