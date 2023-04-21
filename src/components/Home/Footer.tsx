82654a89198b543ca7ae074b75553869839bbf9a
Brage Røsberg <brage70@gmail.com>
2023-04-21 22:12:47 +0200

    refactor: simplify Footer

--- src/components/Home/Footer.tsx
+++ src/components/Home/Footer.tsx
@@ -5,31 +5,30 @@ import moment from 'moment';
 const Footer = () => {
   const { t } = useTranslation();
 
+  const links = [
+    {
+      title: 'Github',
+      url: 'https://www.github.com/bragerosberg',
+    },
+    {
+      title: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/brage-rosberg',
+    },
+    {
+      title: 'E-mail',
+      url: 'mailto:brage@contact.com?subject=Contact from portfolio',
+    },
+  ];
+
   return (
     <ul>
-      <li>
-        <a
-          href="https://www.github.com/bragerosberg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://www.linkedin.com/in/brage-rosberg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-      </li>
-      <li>
-        <a href="mailto:brage@contact.com?subject=Contact from portfolio">
-          E-mail
-        </a>
-      </li>
+      {links.map((link, index) => (
+        <li key={index}>
+          <a href={link.url} target="_blank" rel="noopener noreferrer">
+            {link.title}
+          </a>
+        </li>
+      ))}
       <li className="footer__author">
         {t('home:MadeBy', { year: moment().year() })}
       </li>
