519d2a531ebb90ac79be178fdc758e57a64c9ad2
Brage Røsberg <brage70@gmail.com>
2024-03-30 21:53:43 +0100

    chore: minor changes

--- src/assets/mainTech.ts
+++ src/assets/mainTech.ts
@@ -1,20 +1,20 @@
-import redux from './redux.png';
 import js from './js.png';
 import react from './react.png';
 import nodejs from './nodejs.png';
+import next from './nextjs.jpg';
 import typescript from './typescript.png';
 
 type Tech = {
   name: string;
   logo: string;
-}
+};
 
 const mainTech: Tech[] = [
   { name: 'Typescript', logo: typescript },
   { name: 'JavaScript', logo: js },
   { name: 'React', logo: react },
-  { name: 'Redux', logo: redux },
-  { name: 'Node', logo: nodejs }
+  { name: 'Next', logo: next },
+  { name: 'Node', logo: nodejs },
 ];
 
 export default mainTech;
