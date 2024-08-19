b2287d7c1d42b614e4fe0da586430587fffcf3ae
Brage Røsberg <brage70@gmail.com>
2024-08-19 18:23:30 +0200

    refactor: project page, simplify

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
 import { myProjects } from './projects';
 import './styles/Project.css';
 import { useTranslation } from 'react-i18next';
@@ -8,33 +7,20 @@ const Project = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="project__wrapper">
-      {myProjects.map(
-        ({ image, alt, i18nTitle, i18nText, topics, repolink }, i) => (
-          <Card key={i}>
-            <Card.Img
-              variant="top"
-              className="project__image"
-              src={image}
-              alt={alt}
-            />
-            <Card.Title className="project__title">
-              {t(`project:${i18nTitle}`)}
-            </Card.Title>
-            <Card.Text className="project__text">
-              {t(`project:${i18nText}`)}
-            </Card.Text>
-            <Card.Footer className="project__topic">{topics}</Card.Footer>
-            <div className="button__wrapper">
-              <a href={repolink} target="_blank" rel="noopener noreferrer">
-                <button className="primaryButton">
-                  {t(`project:Repository`)}
-                </button>
-              </a>
-            </div>
-          </Card>
-        )
-      )}
+    <div className="projects">
+      {myProjects.map(({ i18nTitle, i18nText, topics, repolink }, i) => (
+        <div key={i} className="project">
+          <h2>{t(`project:${i18nTitle}`)}</h2>
+          <p>{t(`project:${i18nText}`)}</p>
+          <p>Teknologier: {topics}</p>
+          <p>
+            Source:{' '}
+            <a href={repolink} className="link">
+              Github
+            </a>
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
