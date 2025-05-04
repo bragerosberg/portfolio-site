3b208fe1573207f4d0c86214752f9231aa5ff9b5
bragerosberg <brage70@gmail.com>
2025-05-04 12:50:06 +0200

    feat: add details

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
     const yearsSince2021 = new Date().getFullYear() - 2021;
-    document.getElementById("yoe").innerText = `Fullstack Web Developer (${yearsSince2021} YOE) - Oslo`;
+    document.getElementById("yoe").innerText = `Fullstack Web Developer (${yearsSince2021} YOE)`;
 });
