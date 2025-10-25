2fbacb7868a8b98ddaeadf888451396c242f7833
bragerosberg <brage70@gmail.com>
2025-10-25 14:07:43 +0200

    refactor: inline styles and script

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const yearsSince2021 = new Date().getFullYear() - 2021;
-    document.getElementById("yoe").innerText = `Fullstack Web Developer (${yearsSince2021} YOE)`;
-});
